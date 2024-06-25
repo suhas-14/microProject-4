87da7fb2f5647877ef5a8dfc76ec97206d80d672
suhas <snani4732@gmail.com>
2024-06-25 23:18:53 +0100

    func updates

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const closeIcon = document.querySelector('.close-icon');
 // Add event listener to the rules button
 rulesButton.addEventListener('click', () => {
   // Show the rules popup
-  rulesPopup.style.opacity = '1';
+  rulesPopup.style.display === 'none' ? rulesPopup.style.display = 'block' : rulesPopup.style.display = 'none';
 });
 
 // Add event listener to the close icon
@@ -15,3 +15,5 @@ closeIcon.addEventListener('click', () => {
   // Hide the rules popup
   rulesPopup.style.opacity = '0';
 });
+
+
