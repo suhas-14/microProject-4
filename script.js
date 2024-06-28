48ee79320b2749f73dd391def4b4da4396874131
suhas <snani4732@gmail.com>
2024-06-28 09:42:47 +0100

    media-queries

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 
 // Get the rules button and rules popup elements
-const rulesButton = document.querySelector('.btn-rules');
-const rulesPopup = document.querySelector('.rules-popup');
-const closeIcon = document.querySelector('.close-icon');
+const rulesButton = document.querySelector(".btn-rules");
+const rulesPopup = document.querySelector(".rules-popup");
+const closeIcon = document.querySelector(".close-icon");
 
 // Add event listener to the rules button
-rulesButton.addEventListener('click', () => {
+rulesButton.addEventListener("click", () => {
   // Show the rules popup
-  rulesPopup.style.display === 'none' ? rulesPopup.style.display = 'block' : rulesPopup.style.display = 'none';
+  rulesPopup.style.display === "none"
+    ? (rulesPopup.style.display = "block")
+    : (rulesPopup.style.display = "none");
 });
 
 // Add event listener to the close icon
-closeIcon.addEventListener('click', () => {
+closeIcon.addEventListener("click", () => {
   // Hide the rules popup
-  rulesPopup.style.display = 'none';
+  rulesPopup.style.display = "none";
 });
-
-
